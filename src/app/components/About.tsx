e85b92f5a481ac4b6b9a24395a96feab853eb9d7
studentRabia <pubgali125.com@gmail.com>
2024-11-07 20:13:05 +0500

    eidt

--- src/app/components/About.tsx
+++ src/app/components/About.tsx
@@ -1,12 +1,17 @@
-import React from 'react'
+import React from "react";
 
 const About = () => {
   return (
-    <div id='about' className=' container pt-32'>
-      <h2 className='text-4xl md:text-5xl mb-6'  data-aos="zoom-in-down">About Me</h2>
-      <p className='text-accent text-lg leading-10 text-yellow-100'  data-aos="zoom-in-down" >{`George is a versatile web designer due to his extensive history in graphic and web design. His dedication to continuously learning about new web design trends and concepts has made him a valuable member of the team. With eight years of experience and a master&apos;s in web design, his expertise can help customers modernize with websites and appeal to expanding customer populations.`}</p>
+    <div id="about" className=" container pt-32">
+      <h2 className="text-4xl md:text-5xl mb-6" data-aos="zoom-in-down">
+        About Me
+      </h2>
+      <p
+        className="text-accent text-lg leading-10 text-yellow-100"
+        data-aos="zoom-in-down"
+      >{`George is a versatile web designer due to his extensive history in graphic and web design. His dedication to continuously learning about new web design trends and concepts has made him a valuable member of the team. With eight years of experience and a master's in web design, his expertise can help customers modernize with websites and appeal to expanding customer populations.`}</p>
     </div>
-  )
-}
+  );
+};
 
-export default About
+export default About;
