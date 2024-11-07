42a24819f16cf2a11924a467f4a440f70eed1264
studentRabia <pubgali125.com@gmail.com>
2024-11-07 19:19:22 +0500

    work on about

--- src/app/components/About.tsx
+++ src/app/components/About.tsx
@@ -4,7 +4,7 @@ const About = () => {
   return (
     <div id='about' className=' container pt-32'>
       <h2 className='text-4xl md:text-5xl mb-6'  data-aos="zoom-in-down">About Me</h2>
-      <p className='text-accent text-lg leading-10 text-yellow-100'  data-aos="zoom-in-down" >{`George is a versatile web designer due to his extensive history in graphic and web design. His dedication to continuously learning about new web design trends and concepts has made him a valuable member of the team. With eight years of experience and a master's in web design, his expertise can help customers modernize with websites and appeal to expanding customer populations.`}</p>
+      <p className='text-accent text-lg leading-10 text-yellow-100'  data-aos="zoom-in-down" >George is a versatile web designer due to his extensive history in graphic and web design. His dedication to continuously learning about new web design trends and concepts has made him a valuable member of the team. With eight years of experience and a master's in web design, his expertise can help customers modernize with websites and appeal to expanding customer populations.</p>
     </div>
   )
 }
